de928325f3755405ef44d901afa1179b5bf84e33
Sasha Savenko <sashasavenko3@gmail.com>
2025-10-02 16:03:37 +0300

    notes get fetched

---
+++ app/notes/Notes.client.tsx
@@ -0,0 +1,42 @@
+'use client'
+import css from './NotesClient.module.css'
+
+import Modal from "@/components/Modal/Modal";
+import NoteList from "@/components/NoteList/NoteList";
+import Pagination from "@/components/Pagination/Pagination";
+import SearchBox from "@/components/SearchBox/SearchBox";
+import { fetchNotes } from "@/lib/api";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useDebounce } from "use-debounce";
+
+const NotesClient = ( ) => {
+    const [searchQuery, setSearchQuery] = useState<string>("")
+const [searchValue] = useDebounce(searchQuery, 1000);
+const [isModalOpen, setIsModalOpen] = useState(false)
+const [page, setPage] = useState(1)
+const queryParams = {
+  searchValue,
+  page
+}
+  const {data, isFetching} = useQuery({
+    queryKey: ['notes', searchValue, page],
+    queryFn: () => fetchNotes(queryParams),
+    placeholderData: keepPreviousData
+  })
+const notes = data?.notes ?? [] 
+const totalPages = data?.totalPages ?? 1
+const openModal = () => setIsModalOpen(true);
+const closeModal = () => setIsModalOpen(false)
+    return <div className={css.app}>
+	<header className={css.toolbar}>
+		<SearchBox onChange={setSearchQuery}/>
+    <Pagination totalPages={totalPages} setPage={setPage}/>
+    <button onClick={openModal} className={css.button}>Create note +</button>
+  </header>
+  <NoteList notes={notes} loading={isFetching}/>
+  {isModalOpen && <Modal onClose={closeModal}/>}
+</div>
+}
+
+export default NotesClient
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,40 +1,17 @@
-import { keepPreviousData, useQuery } from "@tanstack/react-query"
-import css from "./App.module.css"
-import { useState } from "react"
-import { useDebounce } from 'use-debounce';
 import { fetchNotes } from "@/lib/api";
-import SearchBox from "@/components/SearchBox/SearchBox";
-import Pagination from "@/components/Pagination/Pagination";
-import NoteList from "@/components/NoteList/NoteList";
-import Modal from "@/components/Modal/Modal";
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import NotesClient from "./Notes.client";
+
 const Notes = async () => {
-const [searchQuery, setSearchQuery] = useState<string>("")
-const [searchValue] = useDebounce(searchQuery, 1000);
-const [isModalOpen, setIsModalOpen] = useState(false)
-const [page, setPage] = useState(1)
-const queryParams = {
-  searchValue,
-  page
-}
-  const {data, isFetching} = useQuery({
-    queryKey: ['notes', searchValue, page],
-    queryFn: () => fetchNotes(queryParams),
-    placeholderData: keepPreviousData
-  })
-const notes = data?.notes ?? [] 
-const totalPages = data?.totalPages ?? 1
-const openModal = () => setIsModalOpen(true);
-const closeModal = () => setIsModalOpen(false)
+const queryClient = new QueryClient();
+await queryClient.prefetchQuery({
+  queryKey: ['notes', '', 1],
+  queryFn: () => fetchNotes({searchValue: '', page: 1})
+})
   return (
-    <div className={css.app}>
-	<header className={css.toolbar}>
-		<SearchBox onChange={setSearchQuery}/>
-    <Pagination totalPages={totalPages} setPage={setPage}/>
-    <button onClick={openModal} className={css.button}>Create note +</button>
-  </header>
-  <NoteList notes={notes} loading={isFetching}/>
-  {isModalOpen && <Modal onClose={closeModal}/>}
-</div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+<NotesClient/>
+    </HydrationBoundary>
   )
 }
 
